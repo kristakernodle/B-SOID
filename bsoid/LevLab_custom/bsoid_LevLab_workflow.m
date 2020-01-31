8ce35aa951588318e51d1a1c52f57fb71e5740eb
kristakernodle <kristakernodle@gmail.com>
2020-01-31 10:03:15 -0500

    Updated through filtering data and model creation.
    Next step: Loading the already saved data and model
    Then: Analysis with the saved stuff

--- bsoid/LevLab_custom/bsoid_LevLab_workflow.m
+++ bsoid/LevLab_custom/bsoid_LevLab_workflow.m
@@ -1,4 +1,4 @@
-function bsoid_LevLab_workflow(csvPath,fps,varargin)
+function [filtData,OF_mdl] = bsoid_LevLab_workflow(csvPath,fps,varargin)
 %BSOID_CUSTOMSETUP    setup DLC output files for use with bsoid
 %
 %   INPUTS:
@@ -6,7 +6,7 @@ function bsoid_LevLab_workflow(csvPath,fps,varargin)
 %              be used with bsoid
 %              csvPath = '/Volumes/SharedX/Neuro-Leventhal/analysis/mouseSkilledReaching/DLCProcessing/B-SOiD/testingData_Center/';
 %              csvPath = 'X:\Neuro-Leventhal\analysis\mouseSkilledReaching\DLCProcessing\B-SOiD\testingData_Center/';
-%   SAVEMATFILE    Logical (true/false) for saving filtData as .mat file in
+%   filterDLCOutput    Logical (true/false) for saving filtData as .mat file in
 %                  csvPath folder
 %
 %   OUTPUTS:
@@ -23,13 +23,13 @@ function bsoid_LevLab_workflow(csvPath,fps,varargin)
     validScalarPosNum = @(x) isnumeric(x) && (x>0);
     addRequired(p,'csvPath',@ischar);
     addRequired(p,'fps',validScalarPosNum);
-    addOptional(p,'saveMatFile',true);
+    addOptional(p,'filterDLCOutput',true);
     addOptional(p,'createModel',true);
     parse(p,csvPath,fps,varargin{:});
     
     csvPath = p.Results.csvPath;
     fps = p.Results.fps;
-    saveMatFile = p.Results.saveMatFile;
+    filterDLCOutput = p.Results.filterDLCOutput;
     createModel = p.Results.createModel;
     
     % Update csvPath formatting
@@ -44,9 +44,9 @@ function bsoid_LevLab_workflow(csvPath,fps,varargin)
     end
     
     %% Load Data
-    if saveMatFile
+    if filterDLCOutput
         % Filter Data
-        filtData = bsoid_customSetup(csvPath,saveMatFile);
+        filtData = bsoid_customSetup(csvPath,filterDLCOutput);
     else
         % Load existing .mat file
         filtData = load([csvPath 'allRawData_*.mat']);
@@ -55,12 +55,21 @@ function bsoid_LevLab_workflow(csvPath,fps,varargin)
     %% Load Model
     if createModel
         % Create unsupervised grouping
-        [f_10fps,tsne_feats,grp,llh,bsoid_fig] = bsoid_gmm(filtData,fps,1);
-        savefig(bsoid_fig,[csvPath 'bsoid_groupingFig.fig']);
+        data = cell(1,length(filtData));
+        for ii = 1:length(filtData)
+            data{1,ii} = filtData{2,ii};
+        end
+        [f_10fps,tsne_feats,grp,llh,bsoid_fig] = bsoid_gmm(data,fps,1);
+        fig1 = gcf;
+        savefig(fig1,[csvPath 'bsoid_groupingFig.fig']);
 
         % Build model for future testing
-        [OF_mdl,CV_amean,CV_asem,acc_fig] = bsoid_mdl(f_10fps,grp);
-        savefig(acc_fig,[csvPath 'bsoid_accuracyFig.fig']);
+        hldout=0.2;
+        cv_it=20;
+        btchsz = floor(length(grp)*0.2/cv_it);
+        [OF_mdl,CV_amean,CV_asem,acc_fig] = bsoid_mdl(f_10fps,grp,hldout,cv_it,btchsz);
+        fig2 = gcf;
+        savefig(fig2,[csvPath 'bsoid_accuracyFig.fig']);
 
         % Save model for future use
         save([csvPath 'BSOID_model_' date '.mat'],'csvPath','fps','filtData','f_10fps','tsne_feats','grp','llh','OF_mdl','CV_amean','CV_asem')
@@ -73,5 +82,7 @@ function bsoid_LevLab_workflow(csvPath,fps,varargin)
             [A,I]=max([allModels(:).datenum]);
         end
     end
+    
+    %% Analyze Data
     
 end
