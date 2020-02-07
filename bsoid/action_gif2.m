b6670252045d7fe25caffd72ae4bdf7c14ee6fc6
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-02-07 07:25:11 -0500

    Added back the '. I need to use row vectors as input rather than column vectors for grp

--- bsoid/action_gif2.m
+++ bsoid/action_gif2.m
@@ -33,7 +33,7 @@ function [t,B,b_ex] = action_gif2(PNGpath,grp,n,n_len,X,filepathOutResults)
     end
     fprintf('Computer generating videos... \nCan human tell me what the animal is doing? \n');
     clear i0 ii t ts act_i_frms ieb x ieb_rnd
-    i0 = [true;diff(grp)~=0];
+    i0 = [true;diff(grp')~=0];
     ii = cumsum(i0);
     t = [0,0;grp(i0)',accumarray(ii,1)];
     ts = cat(2,t(:,1),cumsum(t(:,2)));
